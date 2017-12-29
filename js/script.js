cadd526a8e4fee9bb6d4db56c98da077ab28a3fc
p1416524x <zak.shergill02@gmail.com>
2017-12-29 20:48:16 +0000

    @media tags & validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+//Countdown For events page
+// Sets the date that it is counting until
+var countDownDate = new Date("Feb 5, 2018 20:00:00").getTime();
+
+// Update countdown
+var x = setInterval(function() {
+
+  //current date / time
+  var now = new Date().getTime();
+
+  // distance between
+  var distance = countDownDate - now;
+
+  // calculate the days, hours minutes and seconds left
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Prints the countdown
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Event Finished";
+  }
+}, 1000);
+
+
+
+
+
+//Homepage Slideshow
+
 var slideIndex = 0;
 showSlides();
 
@@ -14,7 +48,7 @@ function showSlides() {
 };
 
 
-
+//Google Maps API
 "use strict";
 
 function initMap() {
@@ -23,13 +57,13 @@ function initMap() {
   let SantaPod = {lat: 52.234850, lng: -0.600091};
   let mapDemo = document.getElementById("map");
 
-  // this sets the default location for when the map is first loaded
+  // default location for when the map is first loaded, and zoom to enable both pointers visiblile
   let map = new google.maps.Map(mapDemo, {
     zoom: 10,
     center: eventloc
   });
 
-  // these set different markers you want to show on your map
+  // two markers on the map for the events
   let markerSilverstone = new google.maps.Marker({
     position: Silverstone,
     map: map,
@@ -42,9 +76,23 @@ function initMap() {
   });
 };
 
-// Scrolls to the top of the document
+
+// Scrolls to the top of the document upon click
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop =
         0;
+};
+
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
